cb582dc183722bcc8650825357181d2bd3eadf90
Jitse Niesen <jitseniesen@yahoo.com>
2018-09-21 10:05:29 +0100

    Add compute_distance function

--- distance.py
+++ distance.py
@@ -1,3 +1,9 @@
 """
 File for computing the minimum distance between points.
 """
+
+import math
+
+def compute_distance(point1, point2):
+    return math.sqrt((point1[0] - point2[0]) ** 2
+                     + (point1[1] - point2[1]) ** 2)
