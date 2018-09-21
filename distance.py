f953d22ea7027be486b6e6adccb4e136f1ea63c9
Jitse Niesen <jitseniesen@yahoo.com>
2018-09-21 10:25:08 +0100

    Add docstring for compute_distance

--- distance.py
+++ distance.py
@@ -5,9 +5,17 @@ File for computing the minimum distance between points.
 import math
 
 def compute_distance(point1, point2):
+    """
+    Compute the distance between two points.
+    
+    The points should be given as a tuple with x and y
+    coordinates. The function returns the distance, which
+    is computed using Pythagoras.
+    """
     return math.sqrt((point1[0] - point2[0]) ** 2
                      + (point1[1] - point2[1]) ** 2)
 
 point1 = (0, 0)
 point2 = (1, 1)
 print(compute_distance(point1, point2))
+print(compute_distance(point1, point1))
