edb9480cca8dbacd923c10daab1138c6e3de9292
Jitse Niesen <jitseniesen@yahoo.com>
2018-09-21 10:38:04 +0100

    Add function for computing minimum distance

--- distance.py
+++ distance.py
@@ -15,7 +15,34 @@ def compute_distance(point1, point2):
     return math.sqrt((point1[0] - point2[0]) ** 2
                      + (point1[1] - point2[1]) ** 2)
 
+def compute_minimum_distance(points):
+    """
+    Compute the minimum distance between several points.
+    
+    Given a list of point, compute the distance between
+    all pairs of points and return the minimum of these
+    distances.
+    """
+    result = None
+    for i in range(len(points)):
+        for j in range(len(points)):
+            if i == j:
+                continue
+            distance = compute_distance(points[i], 
+                                        points[j])
+            if result == None:
+                result = distance
+            elif distance < result:
+                result = distance
+    return result
+            
+ 
 point1 = (0, 0)
 point2 = (1, 1)
 print(compute_distance(point1, point2))
 print(compute_distance(point1, point1))
+
+point3 = (1, 0)
+list_of_points = [point1, point2, point3]
+print(compute_minimum_distance(list_of_points))
+
