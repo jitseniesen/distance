37d28ea71c2102c5d982727d3c7e51ce87f47eb5
Jitse Niesen <jitseniesen@yahoo.com>
2018-09-21 11:50:45 +0100

    Use pytest framework for tests

--- distance.py
+++ distance.py
@@ -35,14 +35,3 @@ def compute_minimum_distance(points):
             elif distance < result:
                 result = distance
     return result
-            
-
-point1 = (0, 0)
-point2 = (1, 1)
-assert compute_distance(point1, point2) == math.sqrt(2)
-assert compute_distance(point1, point1) == 0
-
-point3 = (1, 0)
-list_of_points = [point1, point2, point3]
-assert compute_minimum_distance(list_of_points) == 1
-
